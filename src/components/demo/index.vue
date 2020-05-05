fba12f437a41e913f9af5f235415eb596b426a97
touchpillow <1071721873@qq.com>
2020-05-05 22:13:09 +0800

    dem

--- src/components/demo/index.vue
+++ src/components/demo/index.vue
@@ -1,22 +1,53 @@
 <template>
   <div>
-    <div>{{state.count}}</div>
+    <div>{{state.value}}</div>
     <button @click="clickEvent">click me</button>
+    <div id="content"></div>
+    <span>{{count}}</span>
+    <br />
+    <span>{{status}}</span>
   </div>
 </template>
 <script>
-import { reactive } from "vue";
+import { reactive, watchEffect, onMounted, ref, computed, watch } from "vue";
 export default {
   setup() {
     const state = reactive({
-      count: 0
+      value: 0
     });
+    const count = ref(1);
+    const count1 = ref(1);
     const clickEvent = () => {
-      state.count++;
+      state.value++;
+      count.value++;
     };
+    onMounted(() => {
+      watchEffect(() => {
+        //   console.log(state.value, "post");
+        //   console.log(count.value);
+      });
+      //   watchEffect(() => {
+      //     document.querySelector("#content").innerHTML = new Date().toString();
+      //   });
+    });
+    const status = computed(() => state.value * state.value);
+    watch(status, (newVal, oldVal) => {
+      debugger;
+      console.log("state has change");
+    });
+    // watch(
+    //   [() => state.value, count],
+    //   ([newState, newCount], [oldState, oldCount]) => {
+    //     debugger;
+    //     console.log("state or count has change");
+    //   }
+    // );
     return {
       state,
-      clickEvent
+      clickEvent,
+      count,
+      status,
+      count1
     };
   }
 };
