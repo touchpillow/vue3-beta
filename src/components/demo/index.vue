0f1067a99c5c30d87f2ed71d94a2fddebb34ac01
touchpillow <1071721873@qq.com>
2020-06-01 00:46:48 +0800

    add file

--- src/components/demo/index.vue
+++ src/components/demo/index.vue
@@ -1,54 +1,80 @@
 <template>
-  <div>
-    <div>{{state.value}}</div>
-    <button @click="clickEvent">click me</button>
-    <div id="content"></div>
-    <span>{{count}}</span>
-    <br />
-    <span>{{status}}</span>
-  </div>
+  <div class="node" :style="nodeLayout" @mousedown="mousedownHandle"></div>
 </template>
 <script>
-import { reactive, watchEffect, onMounted, ref, computed, watch } from "vue";
+import { reactive, computed, ref, onMounted, onUnmounted } from "vue";
+// 初始化需要的属性
+const initLayout = () => {
+  const layout = reactive({ x: 0, y: 0 });
+  return {
+    layout
+  };
+};
+// 初始化需要的事件
+const initEvent = layout => {
+  const moveInitLayout = reactive({
+    x: 0,
+    y: 0
+  });
+  const moveLayout = reactive({
+    x: 0,
+    y: 0
+  });
+  const moveFlag = ref(false);
+  const mousedownHandle = e => {
+    e.preventDefault();
+    moveInitLayout.x = layout.x;
+    moveInitLayout.y = layout.y;
+    moveFlag.value = true;
+    moveLayout.x = e.screenX;
+    moveLayout.y = e.screenY;
+  };
+  const moveHandle = e => {
+    e.preventDefault();
+    if (!moveFlag.value) return;
+    layout.x = moveInitLayout.x + e.screenX - moveLayout.x;
+    layout.y = moveInitLayout.y + e.screenY - moveLayout.y;
+  };
+  const mouseupHanlde = e => {
+    e.preventDefault();
+    moveFlag.value = false;
+  };
+  onMounted(() => {
+    window.document.addEventListener("mousemove", moveHandle, false);
+    window.document.addEventListener("mouseup", mouseupHanlde, false);
+  });
+  onUnmounted(() => {
+    window.document.removeEventListener("mousemove", moveHandle);
+    window.document.removeEventListener("mouseup", mouseupHanlde);
+  });
+  return {
+    mousedownHandle
+  };
+};
 export default {
   setup() {
-    const state = reactive({
-      value: 0
+    const { layout } = initLayout();
+    const nodeLayout = computed(() => {
+      return {
+        left: `${layout.x}px`,
+        top: `${layout.y}px`
+      };
     });
-    const count = ref(1);
-    const count1 = ref(1);
-    const clickEvent = () => {
-      state.value++;
-      count.value++;
-    };
-    onMounted(() => {
-      watchEffect(() => {
-        //   console.log(state.value, "post");
-        //   console.log(count.value);
-      });
-      //   watchEffect(() => {
-      //     document.querySelector("#content").innerHTML = new Date().toString();
-      //   });
-    });
-    const status = computed(() => state.value * state.value);
-    watch(status, (newVal, oldVal) => {
-      debugger;
-      console.log("state has change");
-    });
-    // watch(
-    //   [() => state.value, count],
-    //   ([newState, newCount], [oldState, oldCount]) => {
-    //     debugger;
-    //     console.log("state or count has change");
-    //   }
-    // );
+    const { mousedownHandle } = initEvent(layout);
     return {
-      state,
-      clickEvent,
-      count,
-      status,
-      count1
+      nodeLayout,
+      mousedownHandle
     };
   }
 };
 </script>
+<style lang="less" scoped>
+.node {
+  position: absolute;
+  top: 0;
+  left: 0;
+  border: 1px solid #ccc;
+  width: 150px;
+  height: 150px;
+}
+</style>
